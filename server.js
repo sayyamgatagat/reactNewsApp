1267503b56373c96aef6549bd79f6ac831e8483b
sayyam gatagat <sayyamg24@gmail.com>
2021-03-08 22:00:54 +0530

    v2

--- server.js
+++ server.js
@@ -23,6 +23,7 @@ app.get('/api/test', cors(), (req, res) => {
 
   res.json(customers);
 });
-const port = 5000;
+
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => `Server running on port ${port}`);
